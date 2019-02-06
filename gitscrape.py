1612659eeb7035186945610c3a77f7e2df40fe5d
Kevin Morris <kevin.morris@stratodesk.com>
2019-02-05 18:09:23 -0800

    add scrapage of github api
    Signed-off-by: Kevin Morris <kevin.morris@stratodesk.com>

--- gitscrape.py
+++ gitscrape.py
@@ -2,6 +2,7 @@
 import sys
 import requests
 import re
+import json
 
 def usage():
   print("usage: %s 'user/repo'" % sys.argv[0])
@@ -10,6 +11,18 @@ def usage():
 def main():
   if len(sys.argv) == 1 or sys.argv[1].count('/') != 1:
     return usage()
+
+  response = requests.get('https://api.github.com/repos/%s/commits' % sys.argv[1])
+  if response.status_code == 404:
+    print("That repository does not exist on github.")
+    return 2
+  elif response.status_code != 200:
+    print("Github API returned status %d." % response.status_code)
+    return 3
+
+  content = response.content.decode('UTF-8')
+  data = json.loads(content)
+  print(data)
 
   return 0
 
